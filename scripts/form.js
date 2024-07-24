39428d9e46bddbb45d3f19ec6df80c3de127f2ce
Beatriz-Bryant-Jorge <82046660+Beatriz-Bryant-Jorge@users.noreply.github.com>
2024-07-24 22:30:47 +0100

    update form logic

--- scripts/form.js
+++ scripts/form.js
@@ -1,8 +1,13 @@
-const form = document.querySelector("#form");
+const form = document.querySelector("#form-wrapper");
+const contactName = document.getElementById('contact-name');
+const emailAddress = document.getElementById('email-address');
+const messageBody = document.getElementById('message-body');
+
 const submitForm = (e) => {
   e.preventDefault();
   const inputs = document.querySelectorAll('.validate');
   const invalidFields = Array.from(inputs).filter(input => input.value === "");
+  console.log(inputs);
   if (invalidFields.length > 0) {
     invalidFields.forEach((field) => {
       field.classList.add('error')
